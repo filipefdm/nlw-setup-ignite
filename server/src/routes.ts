0378a0cecacf05668a980f11ed73e9b56fc57b4c
Filipe Motta <femotta96@gmail.com>
2023-01-23 13:30:39 -0300

    fix commit changes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -61,10 +61,9 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
 
-    const completedHabits =
-      day?.dayHabits.map((dayHabit) => {
-        return dayHabit.habit_id;
-      }) ?? [];
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
 
     return {
       possibleHabits,
@@ -123,11 +122,11 @@ export async function appRoutes(app: FastifyInstance) {
 
   app.get("/summary", async () => {
     const summary = await prisma.$queryRaw`
-      SELECT
-        D.id,
+      SELECT 
+        D.id, 
         D.date,
         (
-          SELECT
+          SELECT 
             cast(count(*) as float)
           FROM day_habits DH
           WHERE DH.day_id = D.id
